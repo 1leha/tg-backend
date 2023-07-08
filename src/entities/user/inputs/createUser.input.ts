2f7cfc2ad328c8788f01a069b961d4f9c2a13964
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-07-08 15:03:04 +0300

    adding swagger documentation for auth

--- src/entities/user/inputs/createUser.input.ts
+++ src/entities/user/inputs/createUser.input.ts
@@ -1,13 +1,19 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { ApiProperty } from '@nestjs/swagger';
 
 @InputType()
 export class CreateUserInput {
+  @ApiProperty()
   @Field()
   email: string;
 
+  @ApiProperty()
   @Field()
   password: string;
 
   @Field()
   role: string;
+
+  @Field({ nullable: true })
+  token: string;
 }
