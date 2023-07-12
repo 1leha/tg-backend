7f6ae7a8cffca88fa1b2323849df3c26af78eaf7
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-07-12 16:14:48 +0300

    adding tasks service

--- src/entities/task/dto/updateTask.input.ts
+++ src/entities/task/dto/updateTask.input.ts
@@ -2,11 +2,19 @@ import { Field, InputType, Int } from '@nestjs/graphql';
 import { ApiProperty } from '@nestjs/swagger';
 
 @InputType()
-export class UpdateCategoryInput {
+export class UpdateTaskInput {
   @Field((type) => Int)
   id: number;
 
   @ApiProperty()
   @Field()
   name: string;
+
+  @ApiProperty()
+  @Field()
+  dataStart: Date;
+
+  @ApiProperty()
+  @Field()
+  dataEnd: Date;
 }
--- src/entities/task/task.service.ts
+++ src/entities/task/task.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
+import { CategoryService } from '../category/category.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TaskEntity } from './models/task.entity';
+import { Repository } from 'typeorm';
+import { CreateTaskInput } from './dto/createTask.input';
+import { UpdateTaskInput } from './dto/updateTask.input';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(
+    @Inject(forwardRef(() => CategoryService))
+    private readonly categoryService: CategoryService,
+    @InjectRepository(TaskEntity)
+    private readonly taskRepository: Repository<TaskEntity>,
+  ) {}
+
+  async createTask(newTask: CreateTaskInput): Promise<TaskEntity> {
+    return await this.taskRepository.save(newTask);
+  }
+
+  async getAllTasks(): Promise<TaskEntity[]> {
+    return this.taskRepository.find();
+  }
+
+  async getTaskById(id: number): Promise<TaskEntity> {
+    return await this.taskRepository.findOne({ where: { id } });
+  }
+
+  async updateTask(dto: UpdateTaskInput): Promise<TaskEntity> {
+    await this.taskRepository.update({ id: dto.id }, { ...dto });
+
+    const updatedTask = await this.getTaskById(dto.id);
+    return updatedTask;
+  }
+
+  async deleteTask(id: number): Promise<number> {
+    await this.taskRepository.delete(id);
+    return id;
+  }
+}
